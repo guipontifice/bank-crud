78df3f65cfdd5120ef7e30993f1257519b8e77e7
Guilherme <guilhermepontifice.gp@gmail.com>
2025-06-29 16:33:08 -0300

    chore: adicionando pacotes jest e mongodb para testes

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -1,30 +1,15 @@
-const pack = require('./package.json');
-
-const jestTransformer = () => {
-  if (process.env.JEST_TRANSFORMER === 'babel-barrel') {
-    // eslint-disable-next-line
-    console.log('babel-barrel');
-
-    return {
-      '^.+\\.(js|ts|tsx)?$': require.resolve('./babelBarrel'),
+module.exports = {
+  displayName: 'server',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', './dist'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx)?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  testTimeout: 25000,
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json',
+      isolatedModules: true
     }
   }
-
-  // eslint-disable-next-line
-  console.log('babel-jest');
-
-  return {
-    '^.+\\.(js|ts|tsx)?$': 'babel-jest',
-  };
-};
-
-module.exports = {
-  displayName: pack.name,
-  testPathIgnorePatterns: ['/node_modules/', './dist'],
-  resetModules: false,
-  transform: {
-    ...jestTransformer(),
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts|tsx)?$',
-  moduleFileExtensions: ['ts', 'js', 'tsx', 'json'],
 };
