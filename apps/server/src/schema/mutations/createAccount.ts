6df2f524440866407fbb5d7f03401db858674257
Guilherme <guilhermepontifice.gp@gmail.com>
2025-06-30 21:15:11 -0300

    feat: ajustes dos campos de input

--- apps/server/src/schema/mutations/createAccount.ts
+++ apps/server/src/schema/mutations/createAccount.ts
@@ -1,11 +1,24 @@
 import { AccountType } from '../../types/AccountType'
 import { Account } from "../../models/Account"
-import { GraphQLNonNull, GraphQLString } from 'graphql'
+import { GraphQLNonNull, GraphQLString, GraphQLFloat, GraphQLInputObjectType } from 'graphql'
+
+export const CreateAccountInputType = new GraphQLInputObjectType({
+    name: 'CreateAccountInput',
+    fields: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        cpf: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        rg: { type: GraphQLString }, // RG é opcional no seu modelo
+        phone: { type: GraphQLString }, // Phone é opcional no seu modelo
+        monthlyIncome: { type: GraphQLFloat }, // monthlyIncome é opcional no seu modelo
+        initialBalance: { type: GraphQLFloat, defaultValue: 0 }, // Opcional, para um saldo inicial específico
+    },
+});
 
 export const createAccount = {
     type: AccountType,
     args: {
-        input: { type: new GraphQLNonNull(AccountType) }
+        input: { type: new GraphQLNonNull(CreateAccountInputType) }
     },
     resolve: async (_: any, { input }: { input: { name: string; cpf: string; email: string; rg?: string; phone?: string; monthlyIncome?: number; initialBalance?: number } }) => {
         const account = new Account({
