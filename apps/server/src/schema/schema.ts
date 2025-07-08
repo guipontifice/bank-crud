ca91dac86d1708cb87d3f87c3202164faf6f4171
Guilherme <guilhermepontifice.gp@gmail.com>
2025-07-07 21:55:45 -0300

    fix: corrigindo erro no schema ao abrir o localhost

--- apps/server/src/schema/schema.ts
+++ apps/server/src/schema/schema.ts
@@ -2,6 +2,7 @@ import { GraphQLSchema } from 'graphql';
 import { QueryType } from './QueryType'
 import { MutationType } from './MutationType'
 import { SubscriptionType } from './SubscriptionType'
+import '../modules/message/MessageLoader';
 
 export const schema = new GraphQLSchema({
 	query: QueryType,
